50f00150938193158e4435f2a1a8bcf1d6d84808
shohan-pherones <shohan.sub.56@gmail.com>
2023-02-06 21:39:42 +0600

    done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,29 @@
 import SectionTitle from "./SectionTitle";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 const Contact = () => {
+  const formRef = useRef(null);
+
   const sendEmail = (e) => {
     e.preventDefault();
 
     // emailjs integration
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        formRef.current,
+        process.env.REACT_APP_PUBLIC_ID
+      )
+      .then(
+        () => {
+          console.log("Message sent");
+        },
+        () => {
+          console.log("Message not sent");
+        }
+      );
 
     // reset
     e.target.querySelector(".fullname").value = "";
@@ -16,7 +35,11 @@ const Contact = () => {
     <div className="contact container mx-auto mt-40" id="contact">
       <SectionTitle title={"Contact"} />
 
-      <form onSubmit={sendEmail} className="mt-40 grid grid-cols-2 gap-20">
+      <form
+        onSubmit={sendEmail}
+        className="mt-40 grid grid-cols-2 gap-20"
+        ref={formRef}
+      >
         <div className="from-control overflow-hidden">
           <input
             type="text"
